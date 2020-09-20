52b524f8181ecebdfd663eda8c2cc91dd32cd646
iamrichR <44210678+iamrichR@users.noreply.github.com>
2020-09-20 10:11:32 -0400

    cleaned up dailyLog method

--- AppController.py
+++ AppController.py
@@ -15,40 +15,14 @@ class AppController:
     def dailyLog(self):
         quit = False
         while not quit:
-
-            #TODO:  refactor date, task, quantity prompts into separate controller methods
             #TODO:  date entry should be BEFORE the loop
 
-            #prompt date
-            dateStr = self.view.promptDate(self.model.getPrefDateFmt())
-            self.checkForQuit(dateStr)
-            while not (self.model.isDateValid(dateStr)):
-                self.view.showInputError(dateStr)
-                dateStr = self.view.promptDate(self.model.getPrefDateFmt())
-                self.checkForQuit(dateStr)
-            
-            #prompt task
-            #TODO:  allow user to print tasklist if they want
-            taskStr = self.view.promptTask()
-            self.checkForQuit(taskStr)
-            taskExists = self.model.checkForTask(taskStr)
-            if not (taskExists):
-                self.view.errTaskNotFound()
-                #TODO:  allow user to either return to task entry or create new task
-                #TODO:  make a separate logic flow to account for creation of new tasks
-
-            
-            quantitySet = False
-            while not(quantitySet):
-                try:
-                    inputStr = self.view.promptQuantity(self.model.getCurrentTask().title, self.model.getCurrentDate())
-                    quantity = int(inputStr)
-                    quantitySet = True
-                except ValueError:
-                    self.view.showInputError(inputStr)
+            self.getValidDateInput()
+            self.getValidTaskInput()
+            self.getValidQuantityInput()
 
             #TODO:  check for successful log and print a success or failure message
-            self.model.storeDailyLog(quantity)
+            self.model.storeDailyLog()
             self.model.resetDailyLogValues()
 
             #add validation later, response should be either YES/Y or NO/N
@@ -63,7 +37,34 @@ class AppController:
 
             #TODO:  loop daily log until user is done with that day
 
+    def getValidDateInput(self):
+            dateStr = self.view.promptDate(self.model.getPrefDateFmt())
+            self.checkForQuit(dateStr)
+            while not (self.model.isDateValid(dateStr)):
+                self.view.showInputError(dateStr)
+                dateStr = self.view.promptDate(self.model.getPrefDateFmt())
+                self.checkForQuit(dateStr)
 
+    def getValidTaskInput(self):
+        #TODO:  allow user to print tasklist if they want
+        taskStr = self.view.promptTask()
+        self.checkForQuit(taskStr)
+        taskExists = self.model.checkForTask(taskStr)
+        if not (taskExists):
+            self.view.errTaskNotFound()
+            #TODO:  allow user to either return to task entry or create new task
+            #TODO:  make a separate logic flow to account for creation of new tasks
+
+    def getValidQuantityInput(self):
+        quantitySet = False
+        while not(quantitySet):
+            try:
+                inputStr = self.view.promptQuantity(self.model.getCurrentTask().title, self.model.getCurrentDate())
+                quantity = int(inputStr)
+                self.model.setCurrentQuantity(quantity)
+                quantitySet = True
+            except ValueError:
+                self.view.showInputError(inputStr)
 
     def checkForQuit(self, inputStr):
         if inputStr in self.quitCommands:
